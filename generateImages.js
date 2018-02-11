1ffd46f213851a3993eca062d760bd84dd407e70
iver56 <iver56@hotmail.com>
2018-02-11 21:53:57 +0100

    Add script for generating pong images

---
+++ generateImages.js
@@ -0,0 +1,69 @@
+'use strict';
+const Canvas = require('canvas');
+const fs = require('fs');
+const path = require('path');
+const seedrandom = require('seedrandom');
+
+function drawPong(ctx, canvas, leftPaddleY, rightPaddleY, ballX, ballY) {
+  const paddleThickness = 0.03 * canvas.width;
+  const padding = paddleThickness;
+  const paddleLength = 0.15 * canvas.height;
+  const ballRadius = paddleThickness / 2;
+
+  // background
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  // paddles
+  ctx.fillStyle = 'white';
+  ctx.fillRect(
+    padding,
+    leftPaddleY * (canvas.height - paddleLength),
+    paddleThickness,
+    paddleLength
+  );
+  ctx.fillRect(
+    canvas.width - padding - paddleThickness,
+    rightPaddleY * (canvas.height - paddleLength),
+    paddleThickness,
+    paddleLength
+  );
+
+  // ball
+  ctx.beginPath();
+  ctx.arc(
+    padding + paddleThickness + ballRadius + ballX * (canvas.width - 2 * padding - 2 * ballRadius - 2 * paddleThickness),
+    ballRadius + ballY * (canvas.height - 2 * ballRadius),
+    ballRadius,
+    0,
+    2 * Math.PI
+  );
+  ctx.fill();
+}
+
+const prng = seedrandom('deadbeef');
+
+const numImages = 10000;
+for (let i = 0; i < numImages; i++) {
+  let leftPaddleY = prng();
+  let rightPaddleY = prng();
+  let ballX = prng();
+  let ballY = prng();
+
+  let fileName = `pong_${i}.png`;
+  let filePath = path.join(__dirname, 'data', fileName);
+
+  let canvas = new Canvas(32, 32);
+  let ctx = canvas.getContext("2d");
+  drawPong(ctx, canvas, leftPaddleY, rightPaddleY, ballX, ballY);
+
+  let out = fs.createWriteStream(filePath);
+  let stream = canvas.pngStream();
+
+  stream.on('data', function(chunk) {
+    out.write(chunk);
+  });
+  stream.on('end', function(){
+    console.log(`${i}: Saved ${filePath}`);
+  });
+}
