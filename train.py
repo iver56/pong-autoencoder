347d630e8ae8c905d568da11f2e4a5e085ec77b0
iver56 <ij@aiascience.com>
2018-08-16 16:32:50 +0200

    Make an alternative model with convolutional filters

--- train.py
+++ train.py
@@ -16,25 +16,48 @@ data/
 import os
 
 import numpy as np
-from keras.layers import Activation, Dense
+from keras.layers import Activation, Dense, Conv2D, BatchNormalization, MaxPooling2D, Flatten
 from keras.models import Sequential
+from keras.optimizers import SGD
 
 from representation import get_images
-
-train_dir = os.path.join('data', 'training')
-x_train, _ = get_images(train_dir)
-y_train = np.array(x_train)
-
-num_training_samples = len(x_train)
 
 img_width = img_height = 32
 num_pixels = img_width * img_height
 
+train_dir = os.path.join('data', 'training')
+x_train, _ = get_images(train_dir)
+y_train = np.array(x_train).reshape(
+    (
+        len(x_train),
+        num_pixels
+    )
+)
+
+num_training_samples = len(x_train)
+
+input_vector_shape = x_train[0].shape
+print(input_vector_shape)
+
 print('num_train_samples', num_training_samples)
 
 model = Sequential()
-model.add(Dense(64, input_shape=(num_pixels,)))
-model.add(Activation('selu'))
+
+model.add(
+    Conv2D(
+        filters=5,
+        kernel_size=3,
+        strides=1,
+        input_shape=input_vector_shape,
+    )
+)
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=5, kernel_size=3, strides=1))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Flatten())
 model.add(Dense(12))
 model.add(Activation('selu'))
 model.add(Dense(64))
@@ -45,7 +68,7 @@ model.add(Activation('sigmoid'))
 model.compile(
     loss='mean_squared_error',
     optimizer='rmsprop',
-    metrics=['mae']
+    metrics=['mean_absolute_error']
 )
 print(model.summary())
 
@@ -53,7 +76,7 @@ model.fit(
     x_train,
     y_train,
     batch_size=64,
-    epochs=1000
+    epochs=10
 )
 
 model.save('pong-autoencoder.h5')
