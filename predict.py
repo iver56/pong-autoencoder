dff7e22c8d83930df8ce264363677b3846360c86
iver56 <ij@aiascience.com>
2018-08-17 09:10:20 +0200

    Try a model with deconvolutions

--- predict.py
+++ predict.py
@@ -1,8 +1,9 @@
-
 import os
 
+import numpy as np
 from keras.models import load_model
 from skimage.io import imsave
+
 from representation import get_images
 
 validation_dir = os.path.join('data', 'validation')
@@ -16,9 +17,12 @@ num_pixels = img_width * img_height
 print('num_validation_samples', num_validation_samples)
 
 model = load_model('pong-autoencoder.h5')
+
 predictions = model.predict(x_validation)
+
 for i, prediction in enumerate(predictions):
     image = prediction.reshape((img_width, img_height))
+    image = np.clip(image, 0.0, 1.0)
     original_file_path = file_paths[i]
     new_file_path = original_file_path + '.predicted.png'
     imsave(new_file_path, image)
--- train.py
+++ train.py
@@ -16,30 +16,39 @@ data/
 import os
 
 import numpy as np
-from keras.layers import Activation, Dense, Conv2D, BatchNormalization, MaxPooling2D, Flatten
+from keras.layers import (
+    Activation,
+    Conv2D,
+    BatchNormalization,
+    MaxPooling2D,
+    Deconvolution2D,
+)
+from keras.layers import K
 from keras.models import Sequential
-from keras.optimizers import SGD
 
 from representation import get_images
 
 img_width = img_height = 32
 num_pixels = img_width * img_height
 
-train_dir = os.path.join('data', 'training')
+train_dir = os.path.join("data", "training")
 x_train, _ = get_images(train_dir)
-y_train = np.array(x_train).reshape(
-    (
-        len(x_train),
-        num_pixels
-    )
-)
+y_train = np.array(x_train).reshape((len(x_train), img_width, img_height, 1))
 
 num_training_samples = len(x_train)
 
 input_vector_shape = x_train[0].shape
 print(input_vector_shape)
 
-print('num_train_samples', num_training_samples)
+print("num_train_samples", num_training_samples)
+
+
+def clipped_mean_squared_error(y_true, y_pred):
+    """Clip to [0, 1] before calculating the mean squared error."""
+    y_true = K.clip(y_true, K.epsilon(), 1)
+    y_pred = K.clip(y_pred, K.epsilon(), 1)
+    return K.mean(K.square(y_pred - y_true), axis=-1)
+
 
 model = Sequential()
 
@@ -49,34 +58,34 @@ model.add(
         kernel_size=3,
         strides=1,
         input_shape=input_vector_shape,
+        padding="same",
     )
 )
+
 model.add(BatchNormalization())
-model.add(Activation('relu'))
+model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=2))
-model.add(Conv2D(filters=5, kernel_size=3, strides=1))
+model.add(Conv2D(filters=5, kernel_size=3, strides=1, padding="same"))
 model.add(BatchNormalization())
-model.add(Activation('relu'))
-model.add(Flatten())
-model.add(Dense(12))
-model.add(Activation('selu'))
-model.add(Dense(64))
-model.add(Activation('selu'))
-model.add(Dense(num_pixels))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=2))
+model.add(
+    Deconvolution2D(
+        1, kernel_size=(4, 4), strides=(2, 2), padding="same"
+    )
+)
+model.add(
+    Deconvolution2D(
+        1, kernel_size=(4, 4), strides=(2, 2), padding="same"
+    )
+)
 model.add(Activation('sigmoid'))
 
 model.compile(
-    loss='mean_squared_error',
-    optimizer='rmsprop',
-    metrics=['mean_absolute_error']
+    loss='mean_squared_error', optimizer="rmsprop", metrics=["mean_absolute_error"]
 )
 print(model.summary())
 
-model.fit(
-    x_train,
-    y_train,
-    batch_size=64,
-    epochs=10
-)
+model.fit(x_train, y_train, batch_size=32, epochs=10)
 
-model.save('pong-autoencoder.h5')
+model.save("pong-autoencoder.h5")
